938468bb756242855a2580f35f10d4197790ec44
MikeNichols1371 <mnich13711@gmail.com>
2022-07-11 13:41:23 -0400

    Finished Method Overloading Exercise

---
+++ Method Overloading/MethodOverloading.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading
+{
+    internal class MethodOverloading
+    {
+        public static int Add(int a, int b)
+            { return a + b; }   
+        public static decimal Add(decimal c, decimal d)
+            { return c + d; }
+        public static string Add(int e, int f, bool isTrue)
+        {
+            var sum = e + f;
+            if (isTrue == true && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (isTrue == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else if (isTrue == true && sum < 1)
+            {
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return sum.ToString();          
+            } 
+        }  
+    }
+}
---
+++ Method Overloading/Program.cs
@@ -0,0 +1,8 @@
+﻿using Method_Overloading;
+
+int sum = MethodOverloading.Add(1, 2);
+Console.WriteLine(sum);
+decimal sum2 = MethodOverloading.Add(1.3m, 1.6m);
+Console.WriteLine(sum2);
+string sum3 = MethodOverloading.Add(2,4, true);
+Console.WriteLine(sum3);
